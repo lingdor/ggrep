db6c272c41bef362ff64c4aad4f1346439b65590
zhangxiaoxu <31872163@qq.com>
2024-01-23 00:02:33 +0800

    install

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ VERSION=v0.0.2
 
 echo $VERSION
 
-env GOOS=linux GOARCH=amd64 go build -o "bin/ggrep_${VERSION}_linux_x64" ./
+env GOOS=linux GOARCH=amd64 go build -o "bin/ggrep_${VERSION}_linux_x86_64" ./
 env GOOS=linux GOARCH=arm64 go build -o bin/ggrep_${VERSION}_linux_arm64 ./
 env GOOS=darwin GOARCH=arm64 go build -o bin/ggrep_${VERSION}_darwin_arm64 ./
-env GOOS=darwin GOARCH=amd64 go build -o bin/ggrep_${VERSION}_darwin_x64 ./
+env GOOS=darwin GOARCH=amd64 go build -o bin/ggrep_${VERSION}_darwin_x86_64 ./
--- install.sh
+++ install.sh
@@ -2,10 +2,9 @@
 
 kernel=`uname -s|tr A-Z a-z`
 arch=`uname -m`
-
 echo "input your password:"
 
 sudo curl https://github.com/lingdor/ggrep/releases/download/v0.0.2/ggrep_v0.0.1_${kernel}_${arch} -o /usr/local/bin/ggrep
-
+sudo chmod 0751 /usr/local/bin/ggrep
 
 
