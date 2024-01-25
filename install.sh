e099a387e20ace9e98c0737e68c6adeeb47657f8
zhangxiaoxu <31872163@qq.com>
2024-01-25 23:53:32 +0800

    v0.0.5

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ kernel=`uname -s|tr A-Z a-z`
 arch=`uname -m`
 echo "input your password:"
 
-sudo curl https://github.com/lingdor/ggrep/releases/download/v0.0.3/ggrep_v0.0.3_${kernel}_${arch} -o /usr/local/bin/ggrep
+sudo curl "https://github.com/lingdor/ggrep/releases/download/v0.0.6/ggrep_v0.0.6_${kernel}_$arch" -o /usr/local/bin/ggrep
 sudo chmod 0755 /usr/local/bin/ggrep
 
 
